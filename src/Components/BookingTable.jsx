00b6212e2d289f2c4e6e394b74bb3a08876ed2d2
Nithin K <66120456+NiithinK@users.noreply.github.com>
2024-05-20 15:17:35 +0530

    carousel 3

--- src/Components/BookingTable.jsx
+++ src/Components/BookingTable.jsx
@@ -1,27 +1,4 @@
-// // src/components/BookingForm.js
-// import React, { useState } from 'react';
-// import './BookingForm.css'
-// const BookingForm = ({ center, onConfirm }) => {
-//   const [date, setDate] = useState('');
-//   const [time, setTime] = useState('');
 
-//   const handleSubmit = () => {
-//     onConfirm({ center, date, time });
-//   };
-
-//   return (
-//     <div>
-//       <h3>Book Appointment at {center.name}</h3>
-//       <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-//       <input type="time" value={time} onChange={(e) => setTime(e.target.value)} />
-//       <button onClick={handleSubmit}>Confirm</button>
-//     </div>
-//   );
-// };
-
-// export default BookingForm;
-
-// src/components/BasicTable.js
 import React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
